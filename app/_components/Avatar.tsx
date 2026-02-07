ae78cbb6c37d19f3fc173722d2b755d7fc021513
Shudipto Roy <shroy0401@gmail.com>
2026-02-07 20:50:12 +0600

    fixed error

--- app/_components/Avatar.tsx
+++ app/_components/Avatar.tsx
@@ -12,25 +12,31 @@ interface Props {
   creators: Creator[];
 }
 
-export function AvatarComponent({ creators }: Props) {
+export async function AvatarComponent({ creators }: Props) {
   //   console.log(creators);
+
+  // Fetch all member details in parallel
+  const members = await Promise.all(
+    creators.map(c => getMemberDetails(c.creator_id))
+  );
+
   return (
     <div className="*:data-[slot=avatar]:ring-background flex -space-x-2 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:grayscale">
-      {creators.map(async (c) => {
-        const creator = await getMemberDetails(c.creator_id as string);
+      {members.map( (creator) => {
+        // const creator = await getMemberDetails(c.creator_id as string);
         // console.log(creator);
         return (
           <Link
-            href={`/about/${creator[0].id}`}
-            key={c.creator_id}
+            href={`/about/${creator.id}`}
+            key={creator.id}
             className="relative "
           >
             <Avatar className="h-10 w-10 overflow-hidden rounded-full peer ring-2 ring-green-500 ">
-              <AvatarImage src={creator[0].image} alt={creator[0].name} className="object-cover " />
-              <AvatarFallback>{creator[0].name.charAt(0)}</AvatarFallback>
+              <AvatarImage src={creator.image} alt={creator.name} className="object-cover " />
+              <AvatarFallback>{creator.name.charAt(0)}</AvatarFallback>
             </Avatar>
             <p className="min-w-20 absolute -bottom-3 md:-bottom-6 text-[10px] text-white font-thin hidden peer-hover:block ">
-              {creator[0].name}
+              {creator.name}
             </p>
           </Link>
         );
